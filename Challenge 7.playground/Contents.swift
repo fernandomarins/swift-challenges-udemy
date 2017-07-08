e24cb8701861c32a13e788892841a50544fd15c5
Fernando Marins <f.augustomarins@gmail.com>
2017-07-08 12:15:30 -0300

    Created challenge 7

---
+++ Challenge 7.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func condenseWhiteSpace(_ input: String) -> String {
+    
+    
+    return ""
+}
+
+condenseWhiteSpace("a   b   c") // should return a b c
+condenseWhiteSpace("    a") // should return [space]a
+condenseWhiteSpace("abc") // should return abc
